9fffa85818a4b4b375fd3e1843970573fd56014d
JulienBara <bara.julien@yahoo.fr>
2016-10-15 10:11:09 +0100

    Accueil page ready

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,7 @@
+class AdminController < ApplicationController
+  before_action :require_admin
+
+  def index
+  end
+
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  include SessionsHelper
+
+  protected
+    def forbidden
+      render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false)
+    end
+
+    def require_admin
+      unless admin?
+        forbidden
+      end
+    end
+
+    def require_login
+      unless logged_in?
+        redirect_to log_in_url
+      end
+    end
+
+    def require_no_login
+      if logged_in?
+        redirect_to root_url
+      end
+    end
+
 end
