a6dd9cdb6c41f56e5127b7f3857529931cc60ce9
JulienBara <bara.julien@yahoo.fr>
2017-10-16 16:59:02 +0200

    clean

--- test/models/mot_nuage_test.rb
+++ test/models/mot_nuage_test.rb
@@ -2,9 +2,7 @@ require 'test_helper'
 
 class MotNuageTest < ActiveSupport::TestCase
   self.use_transactional_tests = true
-  # test "the truth" do
-  #   assert true
-  # end
+
   test "should not save mot nuage without mot and poids" do
     mot_nuage = MotNuage.new
     assert_not mot_nuage.save
