8a7022017b3e39c0657b4ff8ef131d1b3ae6d698
JulienBara <bara.julien@yahoo.fr>
2017-10-16 16:58:37 +0200

    add unit test

--- test/models/mot_nuage_test.rb
+++ test/models/mot_nuage_test.rb
@@ -10,7 +10,17 @@ class MotNuageTest < ActiveSupport::TestCase
     assert_not mot_nuage.save
   end
 
-  test "save mot nuage without mot and poids" do
+  test "should not save mot nuage without mot" do
+    mot_nuage = MotNuage.new(poids: 20)
+    assert_not mot_nuage.save
+  end
+
+  test "should not save mot nuage without poids" do
+    mot_nuage = MotNuage.new(mot: 'Barbiche')
+    assert_not mot_nuage.save
+  end
+
+  test "save mot nuage" do
     mot_nuage = MotNuage.new(mot: 'Barbiche', poids: 20)
     assert mot_nuage.save
   end
